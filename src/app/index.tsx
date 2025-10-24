15771ba4b6a3893582ba68ec90bb2814655c811c
Jaime Nguyen <jaimenguyen168@gmail.com>
2025-10-24 17:02:17 -0400

    Add maps

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,21 @@
-import { Text, View } from "react-native";
+import { View } from "react-native";
+import MapView from "react-native-maps";
 
 export default function Index() {
   return (
-    <View className="flex-1 items-center justify-center bg-red-500">
-      <Text className="text-2xl font-bold">Edit app/index.tsx to edit this screen.</Text>
+    <View className="flex-1 items-center justify-center">
+      <MapView
+        initialRegion={{
+          latitude: 39.9526,
+          longitude: -75.1652,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+        style={{
+          width: "100%",
+          height: "100%",
+        }}
+      />
     </View>
   );
 }
